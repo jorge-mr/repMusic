18027766dcdf159fd6d7cd827dcf5598b858814d
Jorge MR <jorge_mr@icloud.com>
2018-01-03 21:12:48 -0600

    Listo

--- repMusic/ViewController.swift
+++ repMusic/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var volSlider: UISlider!
+    var reproductor = AVAudioPlayer()
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let rutaAudio = Bundle.main.path(forResource: "Dreams", ofType: "mp3")
+        do {
+            try reproductor = AVAudioPlayer(contentsOf: URL(fileURLWithPath: rutaAudio!))
+            timeSlider.maximumValue = Float(reproductor.duration)
+        } catch {
+            print("ocurrio un error")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playMusic(_ sender: UIButton) {
+        if reproductor.isPlaying {
+            print("ya estaba sonando")
+        }else{
+            reproductor.play()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(actualizaTiempo), userInfo: nil, repeats: true)
+            print("play")
+        }
+    }
+    
+    @IBAction func pauseMusic(_ sender: UIButton) {
+        if reproductor.isPlaying {
+            reproductor.pause()
+            timer.invalidate()
+            print("pause")
+        }else{
+            print("ya estaba en pausa")
+        }
+    }
+    
+    @IBAction func stopMusic(_ sender: UIButton) {
+            reproductor.stop()
+            reproductor.currentTime=0
+            timer.invalidate()
+            timeSlider.value = 0.0
+            print("stop")
+    }
+    
+    @IBAction func changedTimeSlider(_ sender: UISlider) {
+        reproductor.currentTime = TimeInterval(timeSlider.value)
 
+    }
+    
+    @IBAction func changedVolumeSlider(_ sender: UISlider) {
+        reproductor.volume = volSlider.value
+
+    }
+    
+    @objc func actualizaTiempo(){
+        timeSlider.value = Float(reproductor.currentTime)
+    }
+    
 }
 
